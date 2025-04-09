7dc57b9de1c783929fe665927134f1b2388e4ba5
Weickmam Ferreira <weickmam07@gmail.com>
2025-04-09 16:33:00 -0300

    Setup

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-black">
+      Ola mundo
+    </div>
+    // <TooltipProvider>
+      // <BrowserRouter>
+      //   <Router />
+      // </BrowserRouter>
+    //   <Toaster />
+    // </TooltipProvider>
+  )
+}
+
+export default App
